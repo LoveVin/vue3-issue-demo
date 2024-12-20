a11f4bc35b9b6c318daf0d99b46d7423fca74609
lixiaonan <lixiaonan@agree.com.cn>
2024-12-20 15:25:04 +0800

    init

---
+++ vue2-demo/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>vue2-demo</p>
+        <aa-bb name="111" age="22"></aa-bb>
+    </div>
+</template>
+
+<script>
+class MyCustomElement extends HTMLElement {
+  constructor() {
+    super();
+    this.name = "";
+  }
+
+  connectedCallback() {
+    console.log(this.getAttribute("name"))
+    console.log("自定义元素添加至页面。");
+  }
+}
+
+customElements.define("aa-bb", MyCustomElement);
+
+export default {
+    name: "App"
+}
+</script>
---
+++ vue3-demo/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+class MyCustomElement extends HTMLElement {
+  constructor() {
+    super();
+    this.name = "";
+  }
+
+  connectedCallback() {
+    console.log(this.getAttribute("name"))
+    console.log("自定义元素添加至页面。");
+  }
+}
+
+customElements.define("aa-bb", MyCustomElement);
+</script>
+
+<template>
+  <div>
+    vue3-demo
+    <aa-bb name="111" age="22"></aa-bb>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
